d740f4385d7ede20f5742119e467792afea2c241
Bird <zcablii@ucl.ac.uk>
2018-02-21 16:41:37 +0000

    renew

--- midterm/mylib.h
+++ midterm/mylib.h
@@ -3,21 +3,20 @@
 #include "ping.h"
 #include <stdlib.h>
 
-
-typedef struct each {
+typedef struct nodes {
   int leftTicks;
   int rightTicks;
-  struct each *next;
+  struct nodes *nextNode;
 } linkedList;
 
 linkedList *head = NULL;
-
 int preLTicks = 0;
 int preRTicks = 0;
 int speedCorrection;
 double dX = 0;
 double dY = 0;
-double distanceWheels[2]={0,0};
+double distanceWheelsl = 0;
+double distanceWheelsr = 0;
 double coordiante[2]={0,0};
 double coordAngle = 0;
 double currentDistance[2]={0,0};
@@ -28,8 +27,8 @@ void distanceDiff()
 {
   int templist[2];
   drive_getTicks(&templist[0], &templist[1]);
-  distanceWheels[0] = (double) (templist[0]) * 3.249;
-  distanceWheels[1] = (double) (templist[1]) * 3.249;
+  distanceWheelsl = templist[0] * 3.249;
+  distanceWheelsr = templist[1] * 3.249;
 }
 
 double radiusFromMiddle(double currentAngle)
@@ -42,11 +41,20 @@ double radiusFromMiddle(double currentAngle)
   }
 }
 
-void pushTicks(int rightTicks, int leftTicks) {
+void tickDiff(int *leftTicks, int *rightTicks) {
+  int currentLeftTicks, currentRightTicks;
+  drive_getTicks(&currentLeftTicks, &currentRightTicks);
+  *leftTicks = currentLeftTicks - preLTicks;
+  *rightTicks = currentRightTicks - preRTicks;
+  preLTicks = currentLeftTicks;
+  preRTicks = currentRightTicks;
+}
+
+void addTicksList(int rightTicks, int leftTicks) {
   linkedList *new = malloc(sizeof(linkedList));
   (*new).leftTicks = leftTicks;
   (*new).rightTicks = rightTicks;
-  (*new).next = head;
+  (*new).nextNode = head;
   head = new;
 }
 
@@ -54,7 +62,7 @@ void getCoordinates()
 {
   double deltaAngle;
   double r;
-  deltaAngle = (distanceWheels[0] - distanceWheels[1]) / 105.8;
+  deltaAngle = (distanceWheelsl - distanceWheelsr) / 105.8;
   r = radiusFromMiddle(currentAngle);
   if (deltaAngle == 0 || currentAngle == 0) {
     coordiante[0] = currentDistance[0];
@@ -73,10 +81,10 @@ void getCoordinates()
 void calculateCoordinates()
 {
   distanceDiff();
-  currentDistance[1] = distanceWheels[1] - prevDistance[1];
-  currentDistance[0] = distanceWheels[0] - prevDistance[0];
-  prevDistance[1] = distanceWheels[1];
-  prevDistance[0] = distanceWheels[0];
+  currentDistance[1] = distanceWheelsr - prevDistance[1];
+  currentDistance[0] = distanceWheelsl - prevDistance[0];
+  prevDistance[1] = distanceWheelsr;
+  prevDistance[0] = distanceWheelsl;
   getCoordinates(); 
   dY += coordiante[1];
   dX += coordiante[0];
@@ -98,62 +106,47 @@ int infraredDiff() {
   return rightdis - leftdis;
 }
 
-
-void tickDiff(int *leftTicks, int *rightTicks) {
-  int currentLeftTicks, currentRightTicks;
-  drive_getTicks(&currentLeftTicks, &currentRightTicks);
-  *leftTicks = currentLeftTicks - preLTicks;
-  *rightTicks = currentRightTicks - preRTicks;
-  preLTicks = currentLeftTicks;
-  preRTicks = currentRightTicks;
-}
-
-
 void driveForward(int speed) {
 
-  for (int delay_increment = 0; ping_cm(8) > 11; delay_increment++){
-  
+  for (int interval = 0; ping_cm(8) > 11; interval++){
     speedCorrection = infraredDiff() * 0.75;
     drive_speed(speed + speedCorrection, speed - speedCorrection);
-    if (delay_increment % 30 == 0) {
+    if (interval % 30 == 0) 
+    {
       int currentLeft;
       int currentRight;
       tickDiff(&currentLeft, &currentRight);
-      pushTicks(currentRight, currentLeft);
+      addTicksList(currentRight, currentLeft);
       calculateCoordinates();
     }
   }
   drive_speed(0, 0);
 }
 
-
 void goBack(int speed) {
-  int curLeftTicks;
-  int curRightTicks;
-  drive_getTicks(&curLeftTicks, &curRightTicks);
-  preLTicks = curLeftTicks;
-  preRTicks = curRightTicks;
-
-  int currentLeftTicks, currentRightTicks;
+  int currentLeftTicks,currentRightTicks;
   int leftCorrection,rightCorrection;
   int leftTicksLeft, rightTicksLeft;
-  linkedList *iterator = head;
-  while (iterator!= NULL) 
+
+  drive_getTicks(&currentLeftTicks, &currentRightTicks);
+  preLTicks = currentLeftTicks;
+  preRTicks = currentRightTicks;
+  linkedList *iteration = head;
+  while (iteration!= NULL) 
   {
     currentRightTicks = 0;
     currentLeftTicks = 0;
-    rightTicksLeft = (*iterator).rightTicks;
-    leftTicksLeft = (*iterator).leftTicks;
-    while (currentLeftTicks < (leftTicksLeft + 2) 
-            && currentRightTicks < (rightTicksLeft + 2)) {
-    
+    rightTicksLeft = (*iteration).rightTicks;
+    leftTicksLeft = (*iteration).leftTicks;
+    while (currentLeftTicks < (leftTicksLeft + 2)&& currentRightTicks < (rightTicksLeft + 2)) 
+    {
       drive_speed(speed + 3.7 * (currentLeftTicks - (leftTicksLeft + 1)), speed + 3.7 * (currentRightTicks - (rightTicksLeft + 1)));
       drive_getTicks(&currentLeftTicks, &currentRightTicks);
       currentLeftTicks = currentLeftTicks - preLTicks;
       currentRightTicks = currentRightTicks - preRTicks;
     }
     drive_getTicks(&preLTicks, &preRTicks);
-    iterator = (*iterator).next;
+    iteration = (*iteration).nextNode;
   }
   drive_speed(0, 0);
 }
